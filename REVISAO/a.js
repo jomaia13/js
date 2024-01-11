1c7d07ec9bd42e8acf482f17f2ffb56d75d89883
jomaia13 <jomaia13@hotmail.com>
2024-01-11 10:24:49 -0300

    atualizando revisao

--- REVISAO/a.js
+++ REVISAO/a.js
@@ -1,8 +1,28 @@
 // HORA DA REVISAO - VAMOS REVER TODO CONTEUDO ESTUDADO E SEMPRE QUE POSSIVEL 
 // VAMOS USAR COM EXEMPLOS PRATICOS 
 
-let comparacao = '40';
-let resultado = comparacao == 40 ? 'sim' : 'nao';
-console.log(resultado);
+const characters = [
+    { name: 'Frodo', race: 'Hobbit' },
+    { name: 'Sam', race: 'Hobbit' },
+    { name: 'Aragorn', race: 'Man' },
+    { name: 'Legolas', race: 'Elf' },
+    { name: 'Gimli', race: 'Dwarf' },
+    { name: 'Boromir', race: 'Man' },
+    { name: 'Merry', race: 'Hobbit' },
+    { name: 'Pippin', race: 'Hobbit' },
+    { name: 'Gandalf', race: 'Ainur' },
+];
 
-// estudando funcoes
+// let hobbits = []
+// for (key in characters) {
+//     if (characters[key].race === 'Hobbit') {
+//         hobbits.push(characters[key])
+//     }
+// }
+
+const hobbits = characters.filter((character)=>{
+    return character.race === 'Hobbit'
+    
+})
+
+console.log(hobbits)
--- REVISAO/t.js
+++ REVISAO/t.js
@@ -2,8 +2,8 @@
  * @param: t espera uma string
  * @function: t('string') separador de tarefas com titulo...
  */
-t = (t) => { console.log('-'.repeat(15), t, '-'.repeat(15)); }
+t = (t) => { console.log('-'.repeat(15), `${t}`, '-'.repeat(15)); }
 
-export default t();
+export default t;
 
 

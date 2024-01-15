abd40a91e7fcb8a9bd69ba066728a20cad202ea2
jomaia13 <jomaia13@hotmail.com>
2024-01-14 22:14:50 -0300

    alterando revisao

--- 32_array_forEach/a.js
+++ 32_array_forEach/a.js
@@ -1,12 +1,13 @@
+const t = require('../REVISAO/t')
 // como fazer uma iteração(ciclo, laço) em um array
 
-/**
- * ja vimos anteriormente os ciclos (laços) em javascript
- * vamos olhar para o array em especial, é uma coleção de dados podemos fazer uma interação sobre o array
- * 
- */
-console.clear()
+/*
+  ja vimos anteriormente os ciclos (laços) em javascript
+  vamos olhar para o array em especial, é uma coleção de dados podemos fazer uma interação sobre o array
+*/
 
+console.clear()
+t('usando for')
 let nomes = ['vaneilza', 'pamela', 'cristina', 'luciana', 'bruna', 'diana'];
 
 // com o ciclo for

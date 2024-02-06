6372ca400caf70000593b0333c22b798bc667bec
jomaia13 <jomaia13@hotmail.com>
2024-02-06 10:56:11 -0300

    treino com funcao

--- 05_tipos, valores e variaveis/a.js
+++ 05_tipos, valores e variaveis/a.js
@@ -11,3 +11,22 @@
         objetos, arrays, funções 
 
 */
+function isPrime(number) {
+    if (number <= 1) {
+        return false;
+    }
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Testando a função
+const testNumber = 1;
+if (isPrime(testNumber)) {
+    console.log(testNumber + " é primo.");
+} else {
+    console.log(testNumber + " não é primo.");
+}
